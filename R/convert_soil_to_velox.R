9f88c29f0db6986988a0707cfac156ae2f8dfe32
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-23 20:36:24 -0500

    udpates convert to velox function

--- R/convert_soil_to_velox.R
+++ R/convert_soil_to_velox.R
@@ -5,3 +5,15 @@
 #' @examples
 #' convert_soil_to_velox("ph_soil_layer.tif")
 
+convert_soil_to_velox <- function(raster_list){
+
+  veloxLoop <- list()
+  for(i in seq_along(raster_list)){
+    tmp <- velox::velox(raster_list[[i]])
+    #tmp$crop(oafAreaReproject)
+    veloxLoop[[i]] <- tmp
+  }
+
+  return(veloxLoop)
+
+}
