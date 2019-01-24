3aa7d41f46e5871021ac26457efd20572aef98c8
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-23 20:49:27 -0500

    renames crop velox to crop raster

--- R/crop_raster_to_country.R
+++ R/crop_raster_to_country.R
@@ -1,6 +1,6 @@
 #' Take the velox raster and crop the velox objects down to the area where the
 #' SPDF points are
-#' @param veloxRaster The list of velox rasters to be cropped
+#' @param raster_list The original list of raster before converting to velox.
 #' @param country_iso The country iso code to indicate which country the GPS
 #'   points are in to know how to trim the data
 #' @inheritParams match_soil_crs this ensures that the spdf and country polygons
@@ -10,9 +10,12 @@
 #' apply_soil_weights("ph_soil_layer.tif")
 
 
-crop_velox_to_country <- function(veloxRaster, spdf, country_iso){
+crop_velox_to_country <- function(raster_list, spdf, country_iso, data_directory){
 
+  country_map <- intersect_map_and_points(spdf, country_iso, data_directory)
 
+  raster_crop <- lapply(raster_list, function(x){crop(x, extent(country_map))})
+  raster_mask <- lapply(raster_crop, function(x){mask(x, country_map)})
 
-
+  return(raster_mask)
 }
