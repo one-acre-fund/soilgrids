5a329d40529162cc8e67c52d08352d2ee03c6b99
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-23 20:56:20 -0500

    adds map intersect to get viable operating area

---
+++ R/intersect_map_and_points.R
@@ -0,0 +1,22 @@
+#' Create intersected map to use to crop velox rasters.
+#' @param country_iso The country iso code to indicate which country the GPS
+#'   points are in to know how to trim the data
+#' @inheritParams match_soil_crs this ensures that the spdf and country polygons
+#'   match the soil data
+#' @return velox raster cropped to the area where soil points are
+#' @examples
+#' apply_soil_weights("ph_soil_layer.tif")
+
+
+intersect_map_and_points <- function(spdf, country_iso, data_directory){
+
+  spdf <- match_soil_crs(spdf)
+  boundaries_raw <- readRDS(paste0(data_directory, "/GADM_2.8_", country_iso, "_adm2.rds"))
+
+  boundaries <- match_soil_crs(boundaries_raw)
+
+  country_map <- intersect(boundaries, spdf)
+
+  return(country_map)
+
+}
