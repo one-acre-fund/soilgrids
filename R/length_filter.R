32ecb00ec7a57400c817d4e2233758709befc68d
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-28 14:06:10 -0500

    length filter function

---
+++ R/length_filter.R
@@ -0,0 +1,17 @@
+#' Breaks list based on subsets with 3 layers and those with one to handle exceptions in the weights and sum portions.
+#' @param raster_list a list of raster data
+#' @return A list with two elements, the first is the set to be weighted or summed, the second is to be combined with the results of the first.
+#' @examples
+#' apply_soil_weights("ph_soil_layer.tif")
+
+
+length_filter <- function(raster_list){
+
+  elements_with_three <- lapply(raster_list, function(layer) length(layer) == 3)
+
+  layers_to_weigh <- raster_list[unlist(elements_with_three) == TRUE]
+  layers_to_skip <- raster_list[unlist(elements_with_three) == FALSE]
+
+  return(list(layers_to_weigh, layers_to_skip))
+
+}
