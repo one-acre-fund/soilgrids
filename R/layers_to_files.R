0ee164d227def5ef69bf279f4611127d2a5ce8b0
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-28 14:05:04 -0500

    updates reference df of layer names

--- R/layers_to_files.R
+++ R/layers_to_files.R
@@ -9,8 +9,8 @@
 
 layers_to_files <- function(soil_layers){
 
-  file_names <- data.frame(param = c("ph", "carbon", "soil_texture", "sand", "silt", "clay", "CEC"),
-                           file_name = c("af_PHIHOX_T__M", "af_ORCDRC_T__M", "af_TEXMHT_T__M", "af_SNDPPT_T__M", "af_SLTPPT_T__M", "af_CLYPPT_T__M", "af_CEC_T__M"))
+  file_names <- data.frame(param = c("ph", "carbon", "soil_texture", "sand", "silt", "clay", "CEC", "p", "n"),
+                           file_name = c("af_PHIHOX_T__M", "af_ORCDRC_T__M", "af_TEXMHT_T__M", "af_SNDPPT_T__M", "af_SLTPPT_T__M", "af_CLYPPT_T__M", "af_CEC_T__M", "phosphorous_layer_projected", "nitrogen_layer_projected"))
 
   files_to_get <- as.data.frame(file_names[file_names$param %in% soil_layers, ])
   files_to_get$param <- as.character(files_to_get$param)
