044ece5e8313e7bc881748274b6f34723d93c294
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-28 14:10:07 -0500

    adds example

--- R/convert_soil_to_velox.R
+++ R/convert_soil_to_velox.R
@@ -3,7 +3,7 @@
 #' @param soil_raster a raster of soil data from the soil grids repository to be converted to raster for extraction
 #' @return a velox raster version of the soil data for extraction
 #' @examples
-#' convert_soil_to_velox("ph_soil_layer.tif")
+#' convert_soil_to_velox(list("ph_soil_layer.tif". "n_soil_layer.tif))
 
 convert_soil_to_velox <- function(raster_list){
 
