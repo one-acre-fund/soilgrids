5f764558775b43df912d35831669ff27c6d15ca0
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-01-29 10:47:59 -0500

    changes param and adds namespace

--- R/get_country_polygons.R
+++ R/get_country_polygons.R
@@ -1,20 +1,21 @@
 #' checks that country boundaries exist and if they don't downloads them.
+#' @param country_polygon_directory the directory where the shape files live
 #' @note This function is pretty simplistic. It can be simplified by condensing the repeated code.
 #' @return Clarification that all the countries exist as expected in the designated location
 #' @notes This funciton lets people update the shape files from the GADM website.
 #' @export
 
-get_country_polygons <- function(data_directory) {
+get_country_polygons <- function(country_polygon_directory) {
 
   country_iso <- c("KEN", "RWA", "BDI", "TZA", "UGA", "ZMB", "MWI", "ETH", "IND", "NGA")
   country_files <- paste0("GADM_2.8_", country_iso, "_adm2.rds")
 
-  available_files <- list.files(data_directory, pattern = "_adm2")
+  available_files <- list.files(country_polygon_directory, pattern = "_adm2")
   files_to_get <- country_files[!country_files %in% available_files]
 
   countries_to_get <- gsub("GADM_2.8_|_adm2.rds", "", files_to_get)
 
   for(i in seq_along(countries_to_get)){
-    getData("GADM", country=i, level=2, path = data_directory)
+    raster::getData("GADM", country=i, level=2, path = country_polygon_directory)
   }
 }
