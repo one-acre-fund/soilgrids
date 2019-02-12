d204c112729d28627c360478c44886adfff1c005
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-02-12 10:47:03 -0500

    adds soil unit converter to output standard and interpretable values

---
+++ R/convert_soil_units.R
@@ -0,0 +1,14 @@
+#' Creates full raster layers for each soil component, applying weights to each
+#' @param soil_df a list of raster data for each soil property to which to apply the weights
+#' @return the output data set that converts the raw units to more user friendly interpretations
+#' @note This function will live within the master function call to share the output directly.
+#' @examples
+#' get_soil_data(...)
+
+
+convert_soil_units <- function(soil_df){
+
+  return(soil_df %>%
+           dplyr::mutate(ph = ph / 10))
+
+}
