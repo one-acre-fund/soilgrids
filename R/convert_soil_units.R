f0b7c05f384e02c18ecbeddda098161d561926ba
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-03-10 23:00:35 -0400

    adds rlang and fixes small errors in examples

--- R/convert_soil_units.R
+++ R/convert_soil_units.R
@@ -1,5 +1,6 @@
 #' Creates full raster layers for each soil component, applying weights to each
 #' @param soil_df a list of raster data for each soil property to which to apply the weights
+#' @param variable the variable to transform. This function is SUPER SIMPLISTIC.
 #' @return the output data set that converts the raw units to more user friendly interpretations
 #' @note This function will live within the master function call to share the output directly.
 #' @examples
@@ -8,9 +9,9 @@
 
 convert_soil_units <- function(soil_df, variable){
 
-  variable = rlang::enquo(variable)
+  variable = rlang::enquo(variable)f
+  soil_df <- soil_df %>% dplyr::mutate(!!variable := !!variable / 10)
 
-  return(soil_df %>%
-           dplyr::mutate(!!variable = !!variable / 10))
+  return(soil_df)
 
 }
