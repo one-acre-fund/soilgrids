1fdb03e7dd5332a07ff2ca53f299e7250953c3b9
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-03-10 22:29:52 -0400

    adjusts apply soil weights example and fixes other documentation

--- R/convert_soil_to_velox.R
+++ R/convert_soil_to_velox.R
@@ -1,6 +1,6 @@
 #' Converts soil raster to velox
 #'
-#' @param soil_raster a raster of soil data from the soil grids repository to be converted to raster for extraction
+#' @param raster_list a raster of soil data from the soil grids repository to be converted to raster for extraction
 #' @return a velox raster version of the soil data for extraction
 #' @examples
 #' convert_soil_to_velox(list("ph_soil_layer.tif", "n_soil_layer.tif"))
