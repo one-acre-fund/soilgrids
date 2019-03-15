6c7e5a4e9e385f29400e675fc7605701e66d22e0
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-03-15 17:13:45 -0400

    change to soil weights, removes := from convert and deletes hello

--- R/apply_soil_weights.R
+++ R/apply_soil_weights.R
@@ -1,7 +1,8 @@
-#' Creates full raster layers for each soil component, applying weights to each
+#' Creates full raster layers for each soil component applying weights to each
 #' @param raster_list a list of raster data for each soil property to which to apply the weights
 #' @param weight_list the list of weights to apply to those soil layers. Defaults specified and shouldn't need to be altered.
 #' @return a single raster for each soil component with appropriate weights applied
+#' @examples apply_soil_weights(raster_list, weight_list = list(5/30, 10/30, 15/30))
 
 
 apply_soil_weights <- function(raster_list, weight_list = list(5/30, 10/30, 15/30)){
